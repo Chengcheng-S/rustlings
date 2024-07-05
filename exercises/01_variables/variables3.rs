7991be55585a6f6adede504a6eeae5df95a6eee9
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2024-07-05 20:42:03 +0800

    first commit

---
+++ exercises/01_variables/variables3.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x: i32 = 16;
+
+    println!("Number {x}");
+}
---
+++ exercises/13_error_handling/errors6.rs
@@ -0,0 +1,105 @@
+// Using catch-all error types like `Box<dyn Error>` isn't recommended for
+// library code where callers might want to make decisions based on the error
+// content instead of printing it out or propagating it further. Here, we define
+// a custom error type to make it possible for callers to decide what to do next
+// when our function returns an error.
+
+use std::num::ParseIntError;
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+// A custom error type that we will be using in `PositiveNonzeroInteger::parse`.
+#[derive(PartialEq, Debug)]
+enum ParsePosNonzeroError {
+    Creation(CreationError),
+    ParseInt(ParseIntError),
+}
+
+impl ParsePosNonzeroError {
+    fn from_creation(err: CreationError) -> Self {
+        Self::Creation(err)
+    }
+
+    // TODO: Add another error conversion function here.
+    // fn from_parseint(???) -> Self { ??? }
+
+    fn from_parseint(err: ParseIntError) -> Self {
+        Self::ParseInt(err)
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<Self, CreationError> {
+        match value {
+            x if x < 0 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            x => Ok(Self(x as u64)),
+        }
+    }
+
+    fn parse(s: &str) -> Result<Self, ParsePosNonzeroError> {
+        // 使用 match 处理解析整数可能出现的错误，转为自定义错误类型
+        let x_result: Result<i64, ParseIntError> = s.parse();
+        match x_result {
+            Ok(x) => match Self::new(x) {
+                Ok(num) => Ok(num),
+                Err(e) => match e {
+                    CreationError::Negative => {
+                        Err(ParsePosNonzeroError::from_creation(CreationError::Negative))
+                    }
+                    CreationError::Zero => {
+                        Err(ParsePosNonzeroError::from_creation(CreationError::Zero))
+                    }
+                },
+            },
+            Err(err) => Err(ParsePosNonzeroError::from_parseint(err)),
+        }
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_parse_error() {
+        assert!(matches!(
+            PositiveNonzeroInteger::parse("not a number"),
+            Err(ParsePosNonzeroError::ParseInt(_)),
+        ));
+    }
+
+    #[test]
+    fn test_negative() {
+        assert_eq!(
+            PositiveNonzeroInteger::parse("-555"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Negative)),
+        );
+    }
+
+    #[test]
+    fn test_zero() {
+        assert_eq!(
+            PositiveNonzeroInteger::parse("0"),
+            Err(ParsePosNonzeroError::Creation(CreationError::Zero)),
+        );
+    }
+
+    #[test]
+    fn test_positive() {
+        let x = PositiveNonzeroInteger::new(42).unwrap();
+        assert_eq!(x.0, 42);
+        assert_eq!(PositiveNonzeroInteger::parse("42"), Ok(x));
+    }
+}
